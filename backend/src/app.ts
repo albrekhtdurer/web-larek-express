e3896d0e3d7aebe6938fbe37010422acb8452584
Ekaterina Novikova <fearnocait@gmail.com>
2026-01-29 16:22:05 +0300

    [feat] db connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
+
+const dbName = 'weblarek';
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(DB_ADDRESS, { dbName });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+connectToDB();
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
