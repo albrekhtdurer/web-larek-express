57f133052303d28a55f8f5a9f5d09a112c71a424
Ekaterina Novikova <fearnocait@gmail.com>
2026-02-08 19:55:25 +0300

    [fix] logic in post product controller

--- backend/src/controllers/products.ts
+++ backend/src/controllers/products.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import fs from 'fs/promises';
 import { Error as MongooseError } from 'mongoose';
+import path from 'path';
 
 import Product from '../models/product';
 import DefaultError from '../errors/default-error';
@@ -14,16 +16,21 @@ export const createProduct = (req: Request, res: Response, next: NextFunction) =
   const {
     description, image, title, category, price,
   } = req.body;
-  Product.create({
-    description, image, title, category, price,
-  })
-    .then((product) => res.send(product))
-    .catch((err) => {
-      if (err instanceof Error && err.message.includes('E11000')) {
-        next(new ConflictError('Ошибка: товар с таким title уже существует'));
-      } else if (err instanceof MongooseError.ValidationError) {
-        next(new BadRequestError(err.message));
-      }
-      next(new DefaultError(`Ошибка сервера: ${err.message}`));
+  const tmpFileName = path.join(__dirname, `../uploads${image.fileName}`);
+  const newFilename = path.join(__dirname, `../public${image.fileName}`);
+  fs.rename(tmpFileName, newFilename)
+    .then(() => {
+      Product.create({
+        description, image, title, category, price,
+      })
+        .then((product) => res.send(product))
+        .catch((err) => {
+          if (err instanceof Error && err.message.includes('E11000')) {
+            next(new ConflictError('Ошибка: товар с таким title уже существует'));
+          } else if (err instanceof MongooseError.ValidationError) {
+            next(new BadRequestError(err.message));
+          }
+          next(new DefaultError(`Ошибка сервера: ${err.message}`));
+        });
     });
 };
