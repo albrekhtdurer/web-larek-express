20bbbab83da492aac139667cbf3c6b17cccf05cf
Ekaterina Novikova <fearnocait@gmail.com>
2026-02-08 21:31:41 +0300

    [fix] lint

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -2,7 +2,7 @@ import { isCelebrateError } from 'celebrate';
 import { NextFunction, Request, Response } from 'express';
 
 const errorHandler = (
-  (err: any, req: Request, res: Response, next: NextFunction) => {
+  (err: any, _req: Request, res: Response, _next: NextFunction) => {
     let statusCode = err.statusCode || 500;
     if (isCelebrateError(err)) {
       statusCode = 400;
